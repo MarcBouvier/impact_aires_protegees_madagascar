a12ab67a832de40c576d6fc7f90d5a7acdd207b3
fBedecarrats <f.bedecarrats@gmail.com>
2022-09-03 22:44:48 +0200

    Transfer from previous project

---
+++ startup_sspcloud.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Create variables
+WORK_DIR=/home/rstudio/impact_aires_protegees_madagascar
+REPO_URL=https://github.com/fBedecarrats/impact_aires_protegees_madagascar # As initial
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R rstudio:users $WORK_DIR
+
+# copy files from S3 
+mc cp -r s3/fbedecarrats/diffusion/impact_aires_protegees_madagascar/data_s3 $WORK_DIR
+
+# Again to give ritghs also in the data subfolder 
+chown -R rstudio:users $WORK_DIR
+
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && identical(getwd(), path.expand('~')))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('~/impact_aires_protegees_madagascar')
+            rstudioapi::applyTheme('Merbivore')
+            }
+            }, action = 'append')
+            " >> /home/rstudio/.Rprofile
