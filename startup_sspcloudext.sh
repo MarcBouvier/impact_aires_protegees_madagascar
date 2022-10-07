33b312279c5038d0e441e6d8a03b4eb996fc3ad7
Florent Bédécarrats <f.bedecarrats@gmail.com>
2022-10-07 18:05:21 +0200

    same as other startup

--- startup_sspcloudext.sh
+++ startup_sspcloudext.sh
@@ -9,7 +9,7 @@ sudo gdebi quarto-linux-amd64.deb -n
 
 # Create variables
 WORK_DIR=/home/onyxia/work/impact_aires_protegees_madagascar
-REPO_URL=https://github.com/fBedecarrats/impact_aires_protegees_madagascar # As initial
+REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/fBedecarrats/impact_aires_protegees_madagascar.git # As initial
 
 # Git
 git clone $REPO_URL $WORK_DIR
